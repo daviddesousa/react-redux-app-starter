a5d2389fe8747ddc7a86e3088ea999ffd3909502
David De Sousa <david@littlebox.io>
2019-10-22 00:35:20 -0400

    adding the useState() hook

--- src/hooks/hooks_container1.js
+++ src/hooks/hooks_container1.js
@@ -1,11 +1,35 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
+
+
 
 const HooksContainer1 = () => {
-  return (
+
+
+  // const stateValue = useState(0)[0];
+  // const setValue = useState(0)[1];
+  const [stateValue, setValue] = useState(0);
+
+  const incrementValue = () => {
+    setValue(stateValue + 1)
+  }
+
+  const decrementValue = () => {
+    setValue(stateValue - 1)
+  }
+
+  return(
       <div>
         React Hooks
+        <br />
+        <button onClick={ () => incrementValue() }>Inc Local State</button>
+        <button onClick={ () => decrementValue() }>Dec Local State</button>
+        <br />
+        <div>
+          <p>Local State: {stateValue}</p>
+        </div>
       </div>
-  );
-};
+  )
+}
+
 
 export default HooksContainer1;
